3ecfef524378a6f7bf6af15cf7f08ad6a700f236
Louis Tur <louis.tur@gmail.com>
2016-10-26 16:05:08 -0400

    adds in some error handling

--- FacesterGram/Model/User.swift
+++ FacesterGram/Model/User.swift
@@ -8,6 +8,16 @@
 
 import Foundation
 
+internal enum UserModelParseError: Error {
+    case results(json: Any)
+    case name(json: AnyObject)
+    case location(json: AnyObject)
+    case login(json: AnyObject)
+    case id(json: AnyObject)
+    case pictures(json: AnyObject)
+    case email(json: AnyObject)
+}
+
 internal struct User {
     internal let firstName: String
     internal let lastName: String
@@ -28,19 +38,20 @@ internal struct User {
             // 2. begin parsing to our array of user data objects
             guard let response: [String : AnyObject] = jsonData as? [String : AnyObject],
                 let results: [AnyObject] = response["results"] as? [AnyObject] else {
-                return nil
+                throw UserModelParseError.results(json: jsonData)
             }
             
             // 3. Iterrate over each element
             for userResult in results {
-                print("userResult: \(userResult)")
                 
                 // 4. parse out name
                 guard
                     let name: [String : String] = userResult["name"] as? [String : String],
                     let first: String = name["first"],
                     let last: String = name["last"]
-                else { return nil }
+                else {
+                    throw UserModelParseError.name(json: userResult)
+                }
                 
                 // 5. parse out location
                 // ** There will be some problems here: doing [String : String] will occasionally fail since some of the fields are interpreted as Int
@@ -49,31 +60,40 @@ internal struct User {
                     let location: [String : AnyObject] = userResult["location"] as? [String : AnyObject],
                     let city: String = location["city"] as? String,
                     let state: String = location["state"] as? String
-                else { return nil }
+                else {
+                    throw UserModelParseError.login(json: userResult)
+                }
                 
                 // 6. parse out user name
                 guard
                     let login: [String : String] = userResult["login"] as? [String : String],
                     let username: String = login["username"]
-                else { return nil }
+                else {
+                    throw UserModelParseError.login(json: userResult)
+                }
                 
                 // 7. parse out id
                 // ** There will be another problem here: depending on nationality, idValue will be nill **
                 // ** Good place to handle errors gracefully **
                 // ** Options here include making the id optional, or generating a random one, or accepting that this will always return nil
                 guard
-                    let id: [String : String] = userResult["id"] as? [String : String]//,
-                //let idValue: String = id["value"] ?? "N/A"
-                else { return nil }
+                    let id: [String : AnyObject] = userResult["id"] as? [String : AnyObject]//,
+                else {
+                    throw UserModelParseError.id(json: userResult)
+                }
                 
                 // 8. parse out image URLs
                 guard
                     let pictures: [String : String] = userResult["picture"] as? [String : String],
                     let thumbnail: String = pictures["thumbnail"]
-                else { return nil }
+                else {
+                    throw UserModelParseError.pictures(json: userResult)
+                }
                 
                 // 9. the rest
-                guard let email: String = userResult["email"] as? String else { return nil }
+                guard let email: String = userResult["email"] as? String else {
+                    throw UserModelParseError.email(json: userResult)
+                }
                 
                 let validUser: User = User(firstName: first,
                                            lastName: last,
@@ -81,7 +101,7 @@ internal struct User {
                                            state: state,
                                            username: username,
                                            emailAddress: email,
-                                           id: id["value"] ?? "N/A",
+                                           id: (id["value"] as? String) ?? "N/A",
                                            thumbnailURL: thumbnail)
                 
                 usersToReturn?.append(validUser)
@@ -89,8 +109,9 @@ internal struct User {
             
             return usersToReturn
         }
+        // TODO: add better handling of the thrown error
         catch {
-            print("Error encountered with JSONSerialization: \(error)")
+            print("Error encountered with parsing: \(error)")
         }
         
         return nil
